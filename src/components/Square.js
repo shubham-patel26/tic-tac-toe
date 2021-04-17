4ed93cfc3c0de9b856e30021708af6ea33ea35e0
shubham <shubham763116@gmail.com>
2021-04-17 23:01:17 +0530

    taking turns

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,14 @@
 import React,{Component} from 'react';
 
-class Square extends Component{
-   
-
-    render() {
-        // TODO: use onClick={this.props.onClick}
-        // TODO: replace this.state.value with this.props.value
-        return (
+const square=(props)=>{
+    return (
         <button className="square" 
-        onClick={() => this.props.onClick()}
+        onClick={() => props.onClick()}
         >
-            {this.props.value}
+            {props.value}
         </button>
-        );
-    }
+    )
+    
 }
 
 export default Square;
