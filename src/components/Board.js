436cad742dc54837b21eea0077751d51e0420bdf
shubham <shubham763116@gmail.com>
2021-04-17 20:46:06 +0530

    lifting up state

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,9 +8,18 @@ class Board extends Component{
           squares: Array(9).fill(null),
         };
       }
+
+      handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
     
       renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return <Square 
+        value={this.state.squares[i]}
+        onClick={()=>this.handleClick(i)}
+         />;
       }
       
       render() {
@@ -20,13 +29,19 @@ class Board extends Component{
           <div>
             <div className="status">{status}</div>
             <div className="board-row">
-              {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
             </div>
             <div className="board-row">
-              {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
             </div>
             <div className="board-row">
-              {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
             </div>
           </div>
         );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,20 +1,16 @@
 import React,{Component} from 'react';
 
 class Square extends Component{
-    // TODO: remove the constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-        value: null,
-        };
-    }
+   
 
     render() {
         // TODO: use onClick={this.props.onClick}
         // TODO: replace this.state.value with this.props.value
         return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-            {this.state.value}
+        <button className="square" 
+        onClick={() => this.props.onClick()}
+        >
+            {this.props.value}
         </button>
         );
     }
